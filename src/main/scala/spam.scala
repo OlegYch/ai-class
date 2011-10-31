7332101419f29162c5e6ffa1dbc0975c30662308
OlegYch <OlegYch@.(none)>
2011-10-31 23:44:18 +0300

    laplace smoothing

--- src/main/scala/spam.scala
+++ src/main/scala/spam.scala
@@ -30,7 +30,13 @@ object spam extends App {
   val `p(s)` = bd(s.size) / (s ++ h).size
   val `p(h)` = bd(h.size) / (s ++ h).size
 
-  def `p(m|_)`(prob: List[String]) = (bd(1) /: m.flatMap(_.words).map(prob.dict))((i, n) => i * n / prob.dict.values.sum)
+  def `p(m)`(m: String, dict: Map[String, bd]) = dict(m) / dict.values.sum
+
+  def `ls(m)`(m: String, dict: Map[String, bd], allDict: Map[String, bd])(k: bd) = (dict(m) + k) / (dict.values.sum + k * allDict.size)
+
+  def `p(m|_)`(prob: List[String]) = m.flatMap(_.words).map(`p(m)`(_, prob.dict)).product
+
+  def `ls(m|_)`(m: Seq[String])(prob: Seq[String], allProb: Seq[String])(k: bd) = m.flatMap(_.words).map(`ls(m)`(_, prob.dict, allProb.dict)(k)).product
 
   val `p(m|h)` = `p(m|_)`(h)
   val `p(m|s)` = `p(m|_)`(s)
@@ -38,4 +44,16 @@ object spam extends App {
   println(`p(m|h)`)
   println(`p(m|s)`)
   println(`p(s|m)`)
+
+  val k = 1
+
+  def `ls(s)`(p: bd, s: bd) = (s + k) / (p + k * 2)
+
+  //  println(`ls(s)`(1, 1))
+  //  println(`ls(s)`(10, 6))
+  //  println(`ls(s)`(100, 60))
+  println(`ls(s)`((s ++ h).size, s.size))
+  println(`ls(s)`((s ++ h).size, h.size))
+  println(`ls(m|_)`("today" :: Nil)(s, s ++ h)(k))
+  println(`ls(m|_)`("today" :: Nil)(h, s ++ h)(k))
 }
