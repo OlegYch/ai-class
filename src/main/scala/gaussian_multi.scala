cee62657f4b1d6216d94f03f4a8b98e5fe380292
OlegYch <OlegYch@.(none)>
2011-11-07 00:08:44 +0300

    find eigen values

--- src/main/scala/gaussian_multi.scala
+++ src/main/scala/gaussian_multi.scala
@@ -12,7 +12,8 @@ import scalala.tensor.{Matrix, ::}
 object gaussian_multi extends App {
   type bd = Double
   val bd = Double
-  val data = DenseMatrix((3.0, 4.0, 5.0, 6.0, 7.0), (8.0,7.0,5.0,3.0,2.0)).t
+  val data = DenseMatrix((0.0, 1.0, 2.0, 3.0, 4.0), (1.9,3.1,4.0,5.1,5.9)).t
+//  val data = DenseMatrix((3.0, 4.0, 5.0, 6.0, 7.0), (8.0,7.0,5.0,3.0,2.0)).t
   println(data)
   val m = data.numRows
   val k = data.numCols
@@ -29,4 +30,6 @@ object gaussian_multi extends App {
     }
   }
   println("sigma2=" + s)
-}                           
+  println("eig= " + eig(sigma))
+  println("eig= " + eigSym(sigma, true))
+}
