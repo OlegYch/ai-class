766f38b20e40ed91ab3e133eacfbf09712576f31
OlegYch <OlegYch@.(none)>
2011-11-07 03:27:09 +0300

    logic homework

---
+++ src/main/scala/propositional.scala
@@ -0,0 +1,124 @@
+import collection.mutable.Map
+
+object propositional extends App {
+
+  implicit def toVar(s: Symbol) = VarConst(s)
+
+  sealed trait Var {
+    def ==>(r: Var) = new ==>(this, r)
+
+    def <==>(r: Var) = new <==>(this, r)
+
+    def A(r: Var) = new A(this, r)
+
+    def V(r: Var) = new V(this, r)
+
+    def unary_! = new !(this)
+
+    def toBoolean(v: Var = this)(implicit dict: collection.Map[Var, Boolean]): Boolean = v match {
+      case v@(==>(l, r)) => v(toBoolean(l) -> toBoolean(r))
+      case v@(<==>(l, r)) => v(toBoolean(l) -> toBoolean(r))
+      case v@(V(l, r)) => v(toBoolean(l) -> toBoolean(r))
+      case v@(A(l, r)) => v(toBoolean(l) -> toBoolean(r))
+      case v@(!(l)) => v(toBoolean(l))
+      case v@True => true
+      case v@False => false
+      case v: VarConst => dict(v)
+    }
+
+    lazy val vars = {
+      var result = Set[Var]()
+      toBoolean()(Map.empty.withDefault(v => {result += v; false}))
+      result
+    }
+
+    lazy val eval = {
+      for (s <- (0 to vars.size); answer <- vars.toStream.combinations(s).map {
+        trueVars =>
+          implicit val varsMap = vars.map(v => v -> trueVars.contains(v)).toMap
+          (varsMap, toBoolean())
+      }) yield answer
+    }
+
+    lazy val valid = eval.collectFirst {case (_, false) =>}.isEmpty
+    lazy val satisfiable = !valid && eval.collectFirst {case (_, true) =>}.isDefined
+    lazy val unSatisfiable = eval.collectFirst {case (_, true) =>}.isEmpty
+    lazy val `true` = valid
+    lazy val `false` = unSatisfiable
+    lazy val `?` = satisfiable
+  }
+
+  abstract class VarImpl(val s: Symbol) extends Var {
+    override def toString = s.name
+
+    override def equals(obj: Any) = obj match {case v: VarImpl => v.s.equals(s) case _ => false}
+
+    override def hashCode() = s.hashCode()
+  }
+
+  case object True extends VarImpl('True) {
+    def apply = true
+  }
+
+  case object False extends VarImpl('False) {
+    def apply = false
+  }
+
+  case class VarConst(override val s: Symbol) extends VarImpl(s)
+
+  case class ==>(l: Var, r: Var) extends VarImpl(Symbol("(%s ⇒ %s)".format(l, r))) {
+    def apply(p: (Boolean, Boolean)) = p match {
+      case (false, false) => true
+      case (false, true) => true
+      case (true, false) => false
+      case (true, true) => true
+    }
+  }
+
+  case class <==>(l: Var, r: Var) extends VarImpl(Symbol("(%s ⇔ %s)".format(l, r))) {
+    def apply(p: (Boolean, Boolean)) = p match {
+      case (false, false) => true
+      case (false, true) => false
+      case (true, false) => false
+      case (true, true) => true
+    }
+  }
+
+  case class A(l: Var, r: Var) extends VarImpl(Symbol("(%s ∧ %s)".format(l, r))) {
+    def apply(p: (Boolean, Boolean)) = p match {
+      case (false, false) => false
+      case (false, true) => false
+      case (true, false) => false
+      case (true, true) => true
+    }
+  }
+
+  case class V(l: Var, r: Var) extends VarImpl(Symbol("(%s ∨ %s)".format(l, r))) {
+    def apply(p: (Boolean, Boolean)) = p match {
+      case (false, false) => false
+      case (false, true) => true
+      case (true, false) => true
+      case (true, true) => true
+    }
+  }
+
+  case class !(l: Var) extends VarImpl(Symbol("¬%s".format(l))) {
+    def apply(p: Boolean) = !p
+  }
+
+  def inspect(v: Var) = {
+    def toS(b: Boolean) = (if (b) "*" else "\t") + "\t"
+    (v.`true` :: v.`false` :: v.`?` :: Nil).map(toS).mkString("") + v.toString
+  }
+
+  val Smoke = 'Smoke
+  val Fire = 'Fire
+  val Big = 'Big
+  val Dumb = 'Dumb
+  println("True\tFalse\t?\t")
+  println(inspect((Smoke ==> Fire) <==> (Smoke V !Fire)))
+  println(inspect((Smoke ==> Fire) <==> (!Smoke ==> !Fire)))
+  println(inspect((Smoke ==> Fire) <==> (!Fire ==> !Smoke)))
+  println(inspect(Big V Dumb V (Big ==> Dumb)))
+  println(inspect((Big A Dumb) <==> !(!Big V !Dumb)))
+}
