da1774d31f8e12d001d184a331d0dbef7b85c079
OlegYch <OlegYch@.(none)>
2011-11-20 21:57:33 +0300

    midterm

--- src/main/scala/midterm/midterm_5.scala
+++ src/main/scala/midterm/midterm_5.scala
@@ -6,7 +6,7 @@ object midterm_5 extends App with SymbolicVariables {
   type bd = BigDecimal
   val bd = BigDecimal
   "p(l)" =: "p(!l)" =: bd(0.5)
-  "p(hh|!l)" =: ("p(h|!l)" =: bd(0.5)).pow(2)
+  "p(hh|!l)" =: ("p(h|!l)" =: bd(0.5)) * "p(h|!l)"
   "p(hh|l)" =: "p(h|l)" =: bd(1)
   "p(h)" =: "p(h|l)".bd * "p(l)" + "p(h|!l)".bd * "p(!l)"
   "p(hh)" =: "p(hh|l)".bd * "p(l)" + "p(hh|!l)".bd * "p(!l)"
