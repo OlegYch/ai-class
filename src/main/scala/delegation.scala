1d9a831f4c8374fc53d442a6e13567850ca00fa8
OlegYch <OlegYch@.(none)>
2011-11-22 17:20:33 +0300

    delegate to two instances with different type parameters

--- src/main/scala/delegation.scala
+++ src/main/scala/delegation.scala
@@ -4,28 +4,53 @@ trait A {
   def f = 1
 }
 
-object B {
-  implicit def delegateToA(b: B) = b.a
+trait C[T] {
+  def t: T
+
+  def t2(i: T): T
 }
 
-class B(var a: A) {
+object B {
+  implicit def delegateToA(b: B) = b.a
+
+  implicit def delegateToC(b: B) = b.c
+
+  implicit def delegateToC2(b: B) = b.c2
+}
+
+class B(var a: A, var c: C[Int], var c2: C[String]) {
   def qwe = this f
 }
 
-object c extends App {
-  val b = new B(new A {override def f = 2})
+object cApp extends App {
+  val b = new B(new A {override def f = 2}, new C[Int] {
+    def t = 100500
+
+    def t2(i: Int) = i + 100500
+  }, new C[String] {
+    def t = "hello"
+
+    def t2(i: String) = i + " world"
+  })
   println(b.f)
 
   case class ff[T <% A](t: T) {def ff = t.f}
+
   case class fff[T <: A](t: T) {def ff = t.f}
 
   val ff1 = new ff(b) {override def ff = super.ff + t.qwe}
   val ff2 = new fff[A](b) {override def ff = super.ff /*+ t.qwe*/}
-  def q(a: =>A = B.delegateToA(b))=  a.f
+
+  def q(a: => A = B.delegateToA(b)) = a.f
+
   println(q())
   b.a = new A {override def f = 3}
   println(ff1.ff)
   println(ff2.ff)
   println(q())
-  println(q(new A{}))
+  println(q(new A {}))
+  println((b: C[Int]).t)
+  println((b: C[String]).t)
+  println(b.t2(42))
+  println(b.t2("hello"))
 }
