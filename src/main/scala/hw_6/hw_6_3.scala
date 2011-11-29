e435ff9753a148339a9758e5406b12f5c5ebcb21
OlegYch <OlegYch@.(none)>
2011-11-29 21:59:55 +0300

    hw 6 3 (hmm) (double checking with aima)

--- src/main/scala/hw_6/hw_6_3.scala
+++ src/main/scala/hw_6/hw_6_3.scala
@@ -6,38 +6,44 @@ object hw_6_3 extends App with SymbolicVariables {
   type bd = BigDecimal
   val bd = BigDecimal
 
-  "p(a0)" =: "p(b0)" =: bd(0.5)
-  "p(a|a)" =: "p(b|a)" =: "p(b|b)" =: "p(a|b)" =: bd(0.5)
-  "p(x|a)" =: bd(0.1)
-  "p(y|a)" =: bd(0.9)
-  "p(x|b)" =: bd(0.8)
-  "p(y|b)" =: bd(0.2)
-  "p(a)" =: "p(b)" =: bd(0.5)
-  "p(a1)" =: "p(b1)" =: bd(0.5)
-  "p(x0)" =: "p(x|a)".bd * "p(a0)" + "p(x|b)".bd * "p(b0)"
-  "p(x1)" =: "p(x|a)".bd * "p(a1)" + "p(x|b)".bd * "p(b1)"
-  "p(x0x1)" =: "p(x0)".bd * "p(x1)"
+  //example from aima
+  "p(b|a)" =: (bd(1) - ("p(a|a)" =: bd(0.7)))
+  "p(b|b)" =: (bd(1) - ("p(a|b)" =: bd(0.3)))
+  "p(y|a)" =: (bd(1) - ("p(x|a)" =: bd(0.9)))
+  "p(y|b)" =: (bd(1) - ("p(x|b)" =: bd(0.2)))
+  "p(a0)" =: (bd(1) - ("p(b0)" =: bd(0.5)))
+  compute
 
-  "p(x0|a0)" =: bd(1)
+  //example from hw6-3
+  "p(b|a)" =: (bd(1) - ("p(a|a)" =: bd(0.5)))
+  "p(b|b)" =: (bd(1) - ("p(a|b)" =: bd(0.5)))
+  "p(y|a)" =: (bd(1) - ("p(x|a)" =: bd(0.1)))
+  "p(y|b)" =: (bd(1) - ("p(x|b)" =: bd(0.8)))
+  "p(a0)" =: (bd(1) - ("p(b0)" =: bd(0.5)))
+  compute
 
-  "p(x)" =: "p(x|a)".bd * "p(a)" + "p(x|b)".bd * "p(b)"
+  def compute = {
+    "p(a1)" =: (bd(1) - ("p(b1)" =: "p(a0)".bd * "p(b|a)" + "p(b0)".bd * "p(b|b)"))
+    "p(x0)" =: "p(x|a)".bd * "p(a0)" + "p(x|b)".bd * "p(b0)"
+    "p(x1)" =: "p(x|a)".bd * "p(a1)" + "p(x|b)".bd * "p(b1)"
 
-  def norm(a: Variable, b: Variable) = {
-    "alpha" =: bd(1) / (a.bd + b.bd)
-    a =: a.bd * "alpha"
-    b =: b.bd * "alpha"
+    def norm(a: Variable, b: Variable) = {
+      "alpha" =: bd(1) / (a.bd + b.bd)
+      a =: a.bd * "alpha"
+      b =: b.bd * "alpha"
+    }
+
+    "p(a0|x0)" =: "p(x|a)".bd * "p(a0)" / "p(x0)"
+    "p(a0|x0)" =: "p(x|a)".bd * "p(a0)"
+    "p(b0|x0)" =: "p(x|b)".bd * "p(b0)"
+    norm("p(a0|x0)", "p(b0|x0)")
+    "p(a1|x0)" =: "p(x|a)".bd * "p(a1)" / "p(x0)"
+    "p(b1|x0)" =: "p(x|b)".bd * "p(b1)" / "p(x0)"
+    "p(a1|x0)" =: "p(a|a)".bd * "p(a0|x0)" + "p(a|b)".bd * "p(b0|x0)"
+    "p(b1|x0)" =: "p(b|a)".bd * "p(a0|x0)" + "p(b|b)".bd * "p(b0|x0)"
+    norm("p(a1|x0)", "p(b1|x0)")
+    "p(a1|x0x1)" =: "p(x|a)".bd * "p(a1|x0)"
+    "p(b1|x0x1)" =: "p(x|b)".bd * "p(b1|x0)"
+    norm("p(a1|x0x1)", "p(b1|x0x1)")
   }
-
-  "p(a0|x0)" =: "p(x|a)".bd * "p(a0)" / "p(x0)"
-  "p(a0|x0)" =: "p(x|a)".bd * "p(a0)"
-  "p(b0|x0)" =: "p(x|b)".bd * "p(b0)"
-  norm("p(a0|x0)", "p(b0|x0)")
-  "p(a1|x0)" =: "p(x|a)".bd * "p(a1)" / "p(x0)"
-  "p(b1|x0)" =: "p(x|b)".bd * "p(b1)" / "p(x0)"
-  "p(a1|x0)" =: "p(a|a)".bd * "p(a0|x0)" + "p(a|b)".bd * "p(b0|x0)"
-  "p(b1|x0)" =: "p(b|a)".bd * "p(a0|x0)" + "p(b|b)".bd * "p(b0|x0)"
-  norm("p(a1|x0)", "p(b1|x0)")
-  "p(a1|x0x1)" =: "p(x|a)".bd * "p(a1|x0)"
-  "p(b1|x0x1)" =: "p(x|b)".bd * "p(b1|x0)"
-  norm("p(a1|x0x1)", "p(b1|x0x1)")
 }
