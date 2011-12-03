6aae9260b32cf2554424703238e82a0f6dd6f4cf
OlegYch <OlegYch@.(none)>
2011-12-04 00:56:47 +0300

    hw 7 1 perspective projection

--- src/main/scala/hw_7/hw_7_1.scala
+++ src/main/scala/hw_7/hw_7_1.scala
@@ -3,24 +3,24 @@ package hw_7
 import utils.BigDecimalSymbolicVariables
 
 object hw_7_1 extends App with BigDecimalSymbolicVariables {
-  type Dim = Either[bd, pp => bd]
+  type Dim = Either[bd, perspectiveProjection => bd]
   implicit def toLeft(b: bd) = Left(b)
-  implicit def toRight(b: pp => bd) = Right(b)
-  var currentProjection: pp = _
+  implicit def toRight(b: perspectiveProjection => bd) = Right(b)
+  var currentProjection: perspectiveProjection = _
   implicit def fromDim(b: Dim): bd = b match {case Left(b) => b; case Right(b) => b(currentProjection)}
-  case class pp(
-                 x: Dim = (p: pp) => p.X * p.f / p.Z,
-                 f: Dim = (p: pp) => p.x * p.Z / p.X,
-                 X: Dim = (p: pp) => p.x * p.Z / p.f,
-                 Z: Dim = (p: pp) => p.X * p.f / p.x
-                 ) {
+  case class perspectiveProjection(
+                                    x: Dim = (p: perspectiveProjection) => p.X * p.f / p.Z,
+                                    f: Dim = (p: perspectiveProjection) => p.x * p.Z / p.X,
+                                    X: Dim = (p: perspectiveProjection) => p.x * p.Z / p.f,
+                                    Z: Dim = (p: perspectiveProjection) => p.X * p.f / p.x
+                                    ) {
     currentProjection = this
   }
 
   def m = bd(1)
   def mm = bd(1000)
-  "X m" =: pp(Z = 300 * m, f = 100 * mm, x = 1 * mm).X / m
-  "Z m" =: pp(X = 2 * m, f = 40 * mm, x = 1 * mm).Z / m
-  "f mm" =: pp(X = 20 * m, Z = 400 * m, x = 1 * mm).f / mm
-  "x mm" =: pp(X = 10 * m, Z = 100 * m, f = 10 * mm).x / mm
+  "X m" =: perspectiveProjection(Z = 300 * m, f = 100 * mm, x = 1 * mm).X / m
+  "Z m" =: perspectiveProjection(X = 2 * m, f = 40 * mm, x = 1 * mm).Z / m
+  "f mm" =: perspectiveProjection(X = 20 * m, Z = 400 * m, x = 1 * mm).f / mm
+  "x mm" =: perspectiveProjection(X = 10 * m, Z = 100 * m, f = 10 * mm).x / mm
 }
