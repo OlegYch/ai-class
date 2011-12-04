222a035a22c66668e74d9ba48cede2520ecb7027
OlegYch <OlegYch@.(none)>
2011-12-04 23:40:01 +0300

    hw 7 4 (unit 17 10)

--- src/main/scala/hw_7/hw_7_4.scala
+++ src/main/scala/hw_7/hw_7_4.scala
@@ -5,13 +5,20 @@ import utils.{FunctionSystems, BigDecimalSymbolicVariables}
 object hw_7_4 extends App with BigDecimalSymbolicVariables with FunctionSystems {
   type function = StereoDistance
   case class StereoDistance(
-                             Z: Dim = (p: function) => p.f * p.B / (p.x2 - p.x1),
-                             x1: Dim,
-                             x2: Dim,
+                             Z: Dim = (p: function) => p.f * p.B / p.deltaX,
+                             x1: Dim = undef,
+                             x2: Dim = undef,
                              B: Dim,
-                             f: Dim
+                             f: Dim,
+                             deltaX: Dim = (p: function) => {
+                               val fromX = for (x2 <- p.x2.?; x1 <- p.x1.?) yield (x2 - x1)
+                               fromX getOrElse (p.f * p.B / p.Z)
+                             }
                              ) extends FunctionSystem
+
+  def m = bd(1)
   def cm = bd(100)
   def mm = bd(1000)
   "Z cm" =: StereoDistance(f = 8 * mm, x1 = -1 * mm, x2 = 3 * mm, B = 20 * cm).Z / cm
+  "deltaX mm" =: StereoDistance(Z = 10 * m, f = 30 * mm, B = 1 * m).deltaX / mm
 }
--- src/main/scala/utils/FunctionSystems.scala
+++ src/main/scala/utils/FunctionSystems.scala
@@ -8,7 +8,10 @@ trait FunctionSystems {
   implicit def toRight(b: function => bd) = Right(b)
   var currentFunction: () => function = () => sys.error("currentFunction is not assigned")
   implicit def fromDim(b: Dim): bd = b match {case Left(b) => b; case Right(b) => b(currentFunction())}
-
+  val undef = toRight(_ => sys.error("undefined"))
+  implicit def withUndef(d: Dim) = new {
+    def ? = d.left.toOption
+  }
   trait FunctionSystem {self: function =>
     currentFunction = () => self
   }
